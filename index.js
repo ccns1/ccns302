51aa8a9c3faecc2d296866a5577b0064e04ca9ea
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-11-21 13:08:31 +0100

    Added the text chat.

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-var Peer = require('simple-peer')
-var ClipboardJS = require('clipboard')
-var p = new Peer({ initiator: location.hash === '#1', trickle: false })
+let Peer = require('simple-peer')
+let Clipboard = require('clipboard')
+let p = new Peer({ initiator: location.hash === '#1', trickle: false })
 
-p.on('error', function (err) { console.log('[!] There was an error: ', err) })
+p.on('error', (err) => { console.log('[!] There was an error: ', err) })
 
-p.on('signal', function (data) {
+p.on('signal', (data) => {
   document.querySelector('.my-id').value = JSON.stringify(data)
 })
 
-document.querySelector('.connect').addEventListener('click', function (e) {
+document.querySelector('.connect').addEventListener('click', (e)  => {
   e.preventDefault()
+  the_msg.username = username.value.trim() 
   p.signal(JSON.parse(document.querySelector('.other-id').value))
 })
 
-p.on('connect', function () {
-  console.log('CONNECT')
-  p.send('whatever' + Math.random())
+p.on('connect',  () => {
+  console.log('CONNECTED ....')
 })
 
-p.on('data', function (data) {
-  console.log('data: ' + data)
-})
 
 // The clipboard functionality.
-let clipboard = new ClipboardJS('.copy');
+let clipboard = new Clipboard('.copy');
 
-clipboard.on('success', function(e) {
+clipboard.on('success', (e) => {
   console.info('Text:', e.text);
   document.querySelector(".copy").innerHTML = "copied"
   document.querySelector(".my-id").disabled = true
   e.clearSelection();
 });
 
-clipboard.on('error', function(e) {
+clipboard.on('error', (e) => {
   console.error('Action:', e.action);
   console.error('Trigger:', e.trigger);
 });
+
+//adding the messages chating
+let send_btn = document.querySelector("button.send")
+let msg = document.querySelector("textarea.msg")
+let messages = document.querySelector(".messages")
+
+let username = document.querySelector(".username")
+let the_msg = {
+  "username": "",
+  "msg": ""
+}
+send_btn.addEventListener("click", ()=>{
+
+  if( msg.value.trim() != "" ){
+    the_msg.msg = msg.value.trim()
+    p.send(JSON.stringify(the_msg))
+    messages.innerHTML += `<div class="msg m">${the_msg.username}: ${the_msg.msg}</div>`
+  }
+
+})
+
+p.on('data', (data) => {
+  message = JSON.parse(data)
+  messages.innerHTML += `<div class="msg o">${message.username}: ${message.msg}</div>`
+})
